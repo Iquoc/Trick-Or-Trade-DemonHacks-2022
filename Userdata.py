7da31d7623caa6d9f2976b063cd7038f7656e740
JasonBaum <70730136+JasonBaum@users.noreply.github.com>
2022-11-05 17:24:02 -0500

    Update Userdata.py

--- Userdata.py
+++ Userdata.py
@@ -1,17 +1,31 @@
+import tkinter as tk
 
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Trick or Trade")
+
+
+nameVar = tk.StringVar()
+passwordVar = tk.StringVar()
+emailVar = tk.StringVar()
+welcomeLabel = tk.Label(root, text = 'Welcome')
 #use tree if big data set
 users = []
 currentUser = None
+candyTypes = ['Chocolate Bar', 'Lollipop', 'Mint']
+
+
+#class for user data
 class User:
   def __init__(self, name, password, email):
     self.name = name
     self.password = password
     self.email = email
     self.inventory = {}
+    self.preferedCandy = [False, False, False]
 
   def __str__(self):
     return f"{self.name} {self.password} {self.email}"
-    #print("{} {} {}".format(self.name, self.password, self.email))
 
   def setCandy(self, candy, amount):
     self.inventory[candy] = amount
@@ -22,33 +36,73 @@ class User:
   def subtractCandy(self, candy, amount):
     self.inventory[candy] = self.inventory[candy] - amount
 
+  def likedCandy(self,index):
+    self.preferedCandy[index] = True
+
+def clearBoxes():
+  nameVar.set('')
+  passwordVar.set('')
+  emailVar.set('')
+
 def createAccount():
-  name = input("name: ")
-  password = input("password: ")
-  email = input("email: ")
-  users.append(User(name, password,email))
+  name = nameVar.get()
+  password = passwordVar.get().strip()
+  email = emailVar.get().strip()
+  clearBoxes()
+  valid = True
+  if len(name) <= 0 or len(password) <= 0 or len(email) <= 0:
+    valid = False
+  for i in users:
+    if i.name == name:
+      valid = False
+  if(valid):
+    print("creating account #{}".format(len(users)))
+    users.append(User(name, password,email))
+    welcomeLabel.config(text = 'Account Created')
+  else:
+    welcomeLabel.config(text = 'Invalid input')
 
 def signIn():
+  name = nameVar.get()
+  password = passwordVar.get()
+  email = emailVar.get()
+  clearBoxes()
   signedIn = False
-  while(signedIn == False):
-    name = input("username: ")
-    for i in range(len(users)):
-      if users[i].name == name:
-        while(signedIn == False):
-          password = input("password: ")
-          if(users[i].password == password):
-            signedIn = True
-          if(password.lower() == "exit"):
-            break
-          else:
-            print("Incorrect password")
+  for i in range(len(users)):
+    if users[i].name == name:
+      if(users[i].password == password):
+        currentUser = i
+        loadUsersPage()
+        signedIn = True
         break
-    if(signedIn == False):
-      print("User Not Found")
+      else:
+        welcomeLabel.config(text = 'Incorrect password')
+  if signedIn == False:
+    welcomeLabel.config(text = 'User not found')
+
+def loadUsersPage():
+  welcomeLabel.config(text='Welcome, {}'.format(users[currentUser].name))
 
 
-createAccount()
-signIn()
-print(len(users))
-for _ in range(len(users)):
-  print(users[_])
+
+
+nameText = tk.Label(root, text = 'Username')
+nameEntry = tk.Entry(root, textvariable = nameVar)
+passwordText = tk.Label(root, text = 'Password')
+passwordEntry = tk.Entry(root, textvariable = passwordVar)
+emailText = tk.Label(root, text = 'Email')
+emailEntry = tk.Entry(root, textvariable = emailVar)
+signInButton = tk.Button(root, text = 'Sign In', command = signIn)
+createAccButton = tk.Button(root, text = 'Create Account', command = createAccount)
+
+welcomeLabel.grid(row=0, column=1)
+nameText.grid(row=1,column=0)
+nameEntry.grid(row=1,column=1)
+passwordText.grid(row=2,column=0)
+passwordEntry.grid(row=2,column=1)
+emailText.grid(row=3, column = 0)
+emailEntry.grid(row=3, column = 1)
+signInButton.grid(row=4, column=1)
+createAccButton.grid(row=5, column=1)
+
+root.mainloop()
